7966f87b32960521e14ae207ee7b1c413fba695a
111360148 <ruby57930809@gmail.com>
2024-10-01 11:00:06 +0800

    Initial commit

---
+++ Hw1/src/s3/Sample3.java
@@ -0,0 +1,11 @@
+package s3;
+
+public class Sample3 
+{
+  public static void main(String[] args) 
+  {
+    System.out.println('A');
+    System.out.println("歡迎使用Java!");
+    System.out.print(123);
+  }
+}
